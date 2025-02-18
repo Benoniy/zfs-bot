9be7a3c20a556089f35451a00f3cea48ab6b028b
Benoniy <benranson5@gmail.com>
2025-02-18 18:40:59 +0000

    a

--- src/docker.py
+++ src/docker.py
@@ -28,7 +28,7 @@ class Docker(Service):
                 case "stop":
                     await self.discord_client.send_bot_alert("```Stopping```")
                     output = await self.interpret_command("stop", args[1:])
-                    print(output)
+                    print(output == " ".join(args[1:]).strip())
                     if output == " ".join(args[1:]).strip():
                         await self.discord_client.send_bot_alert("```Error stopping container\n{}```".format(output))
                     return True
